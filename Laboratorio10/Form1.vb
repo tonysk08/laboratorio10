82e9e5359d7d3f430d50bea70be0a28ad2b3fc3a
tonysk08 <zarmiento227@gmail.com>
2019-09-23 10:54:17 -0500

    usando metodos, antes de la revision

--- Laboratorio10/Form1.vb
+++ Laboratorio10/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class laboratorio10
 
+    'Desarrollado por Antonios Sarmiento
+    'Desarrollado por Alexis Calderon
     Dim c1, c2, c3, c4 As Int16
     Dim montoTotal As Single = 0
     Const multabase As Single = 50.0
@@ -11,10 +13,9 @@
         c4 = 0
     End Sub
 
-    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
-
-        Dim multa As Single = 0
-        Dim diferenciaDeMulta As Single
+    Public Function ProcesosGenerales()
+        Dim multa As Single = 0 'se controla la multqa individual de la infraccion manejada actualnmente 
+        Dim diferenciaDeMulta As Single 'este contendra el valor extra de la multa en base a las  millas que sobrepaso  
         Try
             If (IsNumeric(txtLimiteDeVelocidad.Text) And IsNumeric(txtVelocicdaReal.Text)) Then
                 If (Val(txtLimiteDeVelocidad.Text) < Val(txtVelocicdaReal.Text)) Then
@@ -26,6 +27,7 @@
                         MsgBox("Ingrese el numero de la avenida y asegure que sea 1,2,3 o 4")
                     Else
                         Select Case Val(txtCuadrante.Text)
+                            'Agrega a la lista de muestra el nombre de la avenidad donde se cometio la infeaccion y suma la cantidad de infracciones en esa avenida
                             Case 1
                                 F_Resultados.txtCuadranteShow.Text = F_Resultados.txtCuadranteShow.Text + "Ave.Frangipani" + vbCrLf
                                 c1 += 1
@@ -40,12 +42,19 @@
                                 c4 += 1
                         End Select
 
+                        'monto total es la suma total de todas la multas registradas (variable global)
                         montoTotal += multa
+
+                        'agraga todos los valores para ser mostrados
                         F_Resultados.txtNplacashow.Text = F_Resultados.txtNplacashow.Text + txtPlaca.Text + vbCrLf
                         F_Resultados.txtLimiteShow.Text = F_Resultados.txtLimiteShow.Text + txtLimiteDeVelocidad.Text + vbCrLf
                         F_Resultados.txtVelocidadShow.Text = F_Resultados.txtVelocidadShow.Text + txtVelocicdaReal.Text + vbCrLf
                         F_Resultados.txtMultasShow.Text = F_Resultados.txtMultasShow.Text & multa & vbCrLf
                         F_Resultados.txtTotal.Text = montoTotal
+
+                        LimppiarCajas()
+
+                        MsgBox("Registro agregado satisfactoriamente")
 
                     End If
                 Else
@@ -57,6 +66,19 @@
         Catch ex As Exception
             MsgBox("Revise los campos y corrija, ingrese solo numeros en las velocidaddes")
         End Try
+    End Function
+
+    Public Function LimppiarCajas()
+        txtCuadrante.Clear()
+        txtLimiteDeVelocidad.Clear()
+        txtPlaca.Clear()
+        txtVelocicdaReal.Clear()
+    End Function
+
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        ProcesosGenerales()
+
     End Sub
 
     Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
